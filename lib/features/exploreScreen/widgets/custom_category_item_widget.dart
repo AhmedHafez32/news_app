01905f55440257c28e73d25e6ffced6a1eec14a5
AhmedHafez32 <mohahz38556@gmai.com>
2025-09-27 04:18:51 +0300

    add CustomCategoryItemWidget with image, title, author, and date

---
+++ lib/features/exploreScreen/widgets/custom_category_item_widget.dart
@@ -0,0 +1,58 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/core/styles/app_styling.dart';
+import 'package:news_app/core/widgets/spacing_widgets.dart';
+
+class CustomCategoryItemWidget extends StatelessWidget {
+  final String? imageUrl;
+  final String title;
+  final String authorName;
+  final String date;
+  const CustomCategoryItemWidget({
+    super.key,
+    this.imageUrl,
+    required this.title,
+    required this.authorName,
+    required this.date,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: CachedNetworkImage(
+            imageUrl:
+                imageUrl ??
+                'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?cs=srgb&dl=pexels-pixabay-158651.jpg&fm=jpg',
+            height: 206,
+            width: 366,
+            fit: BoxFit.fill,
+          ),
+        ),
+        const HeightSpace(height: 16),
+        SizedBox(
+          width: 366,
+          child: Text(
+            title,
+            style: AppStyles.black32SemiboldStyle.copyWith(fontSize: 18),
+          ),
+        ),
+        const HeightSpace(height: 12),
+        Row(
+          children: [
+            Text(
+              '$authorName . $date',
+              style: AppStyles.gray14Regular.copyWith(
+                fontSize: 12,
+                color: Color(0xff6D6265),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
