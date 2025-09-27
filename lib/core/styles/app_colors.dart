5d115a8d9e855da1da73819a699efce61880ab60
AhmedHafez32 <mohahz38556@gmai.com>
2025-09-27 03:22:25 +0300

    update app_colors.dart

--- lib/core/styles/app_colors.dart
+++ lib/core/styles/app_colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/animation.dart';
 
 class AppColors {
   static const Color whiteColor = Color(0xffFFFFFF);
-  static const Color blackColor = Color(0xff101010);
+  static const Color blackColor = Color(0xff231F20);
   static const Color grayColor = Color(0xffE9EEFA);
 }
